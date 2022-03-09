535c6edc12eb5d84e555a06125e5281b85e5e2b9
Omar Melendez <omar.mel.lopez@gmail.com>
2022-03-09 03:59:27 -0600

    add: ajustes del test.js

--- pruebas-unitarias/calculadora.test.js
+++ pruebas-unitarias/calculadora.test.js
@@ -1,5 +1,4 @@
-const { sumar } = require("./calculadora");
-const { restar } = require("./calculadora");
+const { sumar, restar, multiplicar, dividir } = require("./calculadora");
 //Prueba unitaria
 // describe("Pruebas en el componente calculadora", () => {
 // 	test("Prueba funcion sumar", () => {
@@ -8,10 +7,16 @@ const { restar } = require("./calculadora");
 // });
 
 describe("Prueba en componente calculadora", () => {
+	test("Prueba funcion sumar", () => {
+		expect(sumar(4, 6)).toBe(10);
+	});
 	test("Prueba funcion restar", () => {
 		expect(restar(30, 10)).toBe(20);
 	});
-	test("Prueba funcion sumar", () => {
-		expect(sumar(3, 4)).toBe(7);
+	test("Prueba funcion multiplicar", () => {
+		expect(multiplicar(30, 10)).toBe(300);
+	});
+	test("Prueba funcion dividir", () => {
+		expect(dividir(30, 10)).toBe(3);
 	});
 });
